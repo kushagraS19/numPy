ff440cfc68f3512e4c717c7036257c0bcc0f85d8
Kushagra Saini <kushagra.saini19@gmail.com>
2026-09-02 21:44:20 +0530

    Completed student grade analyzer

--- PROJECT/student_grade_analyzer.py
+++ PROJECT/student_grade_analyzer.py
@@ -7,5 +7,40 @@ students = np.array([
 ])
 
 names = students[:,0]
-marks = students[:,1].astype(float)
+marks = students[:,1:].astype(float)
 
+total = np.sum(marks,axis = 1)
+average = np.round(np.mean(marks, axis = 1),2)
+
+print(total, " ", average)
+
+for i in range(len(names)):
+    print(names[i],total[i],average[i])
+
+def assign_grades(avg):
+    if avg >= 90:
+        grade = 'A+'
+    elif avg >= 80:
+        grade = 'A'
+    elif avg >= 70:
+        grade = 'B+'
+    elif avg >= 60:
+        grade = 'B'
+    elif avg >= 50:
+        grade = 'C+'
+    else :
+        grade = 'F'
+
+    return grade
+
+grades = [assign_grades(avg) for avg in average]
+
+topper_index = np.argmax(average)
+print("Topper :", names[topper_index])
+
+class_average = np.round(np.mean(marks))
+print("class average :", class_average)
+
+print("GRADES --> ")
+for i in range(len(names)):
+    print(names[i], ":", grades[i])
